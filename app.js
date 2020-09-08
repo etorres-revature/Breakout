a5d27d523b2d04b8989ba217e46d2d3d520c344d
Eric D. Torres <etorresnotary@gmail.com>
2020-09-08 17:01:47 -0500

    open/close rules funcitonal

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-const
+const rulesBtn = document.querySelector("#rules-btn");
+const closeBtn = document.querySelector("#close-btn");
+const rulesEl = document.querySelector("#rules");
+
+//rules and close event handlers
+rulesBtn.addEventListener("click", () => rulesEl.classList.add("show"));
+closeBtn.addEventListener("click", () => rulesEl.classList.remove("show"));
