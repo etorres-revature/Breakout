2df0e058df0747fa8bb6cade03d0e5d03c890bce
Eric D. Torres <etorresnotary@gmail.com>
2020-09-08 17:25:38 -0500

    draw paddle and draw ball

--- app.js
+++ app.js
@@ -1,6 +1,52 @@
+//canvas API
+//https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API
+
 const rulesBtn = document.querySelector("#rules-btn");
 const closeBtn = document.querySelector("#close-btn");
 const rulesEl = document.querySelector("#rules");
+const canvasEl = document.querySelector("#canvas");
+const ctx = canvas.getContext("2d");
+
+//create ball properties
+const ball = {
+  x: canvasEl.width / 2,
+  y: canvasEl.height / 2,
+  size: 10,
+  speed: 4,
+  dx: 4,
+  dy: -4,
+};
+
+//create paddle properties
+const paddle = {
+  x: canvasEl.width / 2 - 40,
+  y: canvasEl.height - 20,
+  w: 80,
+  h: 10,
+  speed: 8,
+  dx: 0,
+};
+
+//draw ball onto canvas
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2);
+  ctx.fillStyle = "#333";
+  ctx.fill();
+  ctx.closePath();
+}
+
+//draw paddle on canvas
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h);
+  ctx.fillStyle = "#333";
+  ctx.fill();
+  ctx.closePath();
+}
+
+drawBall();
+drawPaddle();
 
 //rules and close event handlers
 rulesBtn.addEventListener("click", () => rulesEl.classList.add("show"));
